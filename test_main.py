da7bd0f085d4e385dc986a89960f2b84f04c06af
Derek Chen <derex1987@gmail.com>
2023-06-13 17:05:57 +0800

    cipher.txt test

--- test_main.py
+++ test_main.py
@@ -7,10 +7,15 @@ from cryptography.hazmat.primitives import padding
 
 key = b'\x01\x23\x45\x67\x89\xab\xcd\xef\xfe\xdc\xba\x98\x76\x54\x32\x10' \
       b'\x01\x23\x45\x67\x89\xab\xcd\xef\xfe\xdc\xba\x98\x76\x54\x32\x10'
-plaintext = b'Your next line is...'
+plain_text = b'Your next line is...'
 
-print("Plaintext:", plaintext)
-ciphertext = encode.encode_func(plaintext, key)
-print("Ciphertext:", ciphertext)
-decrypted_text = decode.decode_func(ciphertext, key)
+print("Plaintext:", plain_text)
+cipher_text = encode.encode_func(plain_text, key)
+print("Ciphertext:", cipher_text)
+
+# Open the file in write mode
+with open("cipher.txt", "wb") as file:
+    file.write(cipher_text)
+print("Cipher text saved to file.")
+decrypted_text = decode.decode_func(cipher_text, key)
 print("Decrypted Text:", decrypted_text)
