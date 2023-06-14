fdba3c1c0b1dd0461a3c54c435d952ee8a3b9e58
Derek Chen <derex1987@gmail.com>
2023-06-14 11:04:26 +0800

    Horray works with photos

--- test_main.py
+++ test_main.py
@@ -9,15 +9,18 @@ key = b'\x01\x23\x45\x67\x89\xab\xcd\xef\xfe\xdc\xba\x98\x76\x54\x32\x10' \
       b'\x01\x23\x45\x67\x89\xab\xcd\xef\xfe\xdc\xba\x98\x76\x54\x32\x10'
 plain_text = open("toEncode", "rb").read()
 
-print("Plaintext:", plain_text)
+# print("Plaintext:", plain_text)
 cipher_text = encode.encode_func(plain_text, key)
-print("Ciphertext:", cipher_text)
+# print("Ciphertext:", cipher_text)
 
 # Open the file in write mode
 with open("cipher.txt", "wb") as file:
     file.write(cipher_text)
-print("Cipher text saved to file.")
+    print("Cipher text saved to file.")
+
 decrypted_text = decode.decode_func(cipher_text, key)
-print("Decrypted Text:", decrypted_text)
+# print("Decrypted Text:", decrypted_text)
+
 with open("decoded", "wb") as file:
     file.write(decrypted_text)
+    print("Decrypted file")
