69874edb64a77bae8f464610704e35f2696a1432
Derek Chen <derex1987@gmail.com>
2023-06-15 16:11:49 +0800

    We will now try this one more time.

--- decode.cpp
+++ decode.cpp
@@ -51,11 +51,24 @@ int remove_padding(unsigned char* decrypted_text, int text_length) {
 // }
 
 int main() {
-    std::ifstream ifs("cipher.txt", std::ios::binary|std::ios::ate);
-    std::ifstream::pos_type pos = ifs.tellg();
-    unsigned char * ciphertext[pos];
-    ifs.seekg(0, std::ios::beg);
-    ifs.read((char *) ciphertext[0], pos);
+    std::ifstream file("cipher.txt", std::ios::in | std::ios::binary);
+    
+    // Get file size
+    file.seekg(0, std::ios::end);
+    std::streampos file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    unsigned char ciphertext[file_size];
+    file.read((char *) ciphertext, file_size);
+
+
+
+
+
+
+
+
+
 
     unsigned char key[32]= { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,
                     0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10,
@@ -68,7 +81,7 @@ int main() {
         iv[i] = 0;
     }
 
-    aes_256_decrypt(*ciphertext, key, iv, plaintext);
+    aes_256_decrypt(ciphertext, key, iv, plaintext);
 
     /* Show the decrypted text */
     printf("Decrypted text: %s\n", plaintext);
