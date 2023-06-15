b6d56b094f338110ce268d8ab39f6d8742bc8e3b
Derek Chen <derex1987@gmail.com>
2023-06-15 16:57:15 +0800

    Trying to use EVP BUT SAME RESULTS TWAFSDA

--- decode.cpp
+++ decode.cpp
@@ -1,5 +1,6 @@
 #include <openssl/aes.h>
 #include <openssl/rand.h>
+#include <openssl/evp.h>
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -9,10 +10,17 @@
  * @brief  Decrypts the given AES ciphertext using the provided key
  * 
  */
-void aes_256_decrypt(unsigned char *ciphertext, unsigned char *key, unsigned char *iv, unsigned char *plaintext) {
+void aes_256_decrypt(unsigned char *ciphertext, int cipher_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext) {
     AES_KEY aes_key;
-    AES_set_decrypt_key(key, 256, &aes_key);
-    AES_cbc_encrypt(ciphertext, plaintext, AES_BLOCK_SIZE, &aes_key, iv, AES_DECRYPT);
+    int len;
+
+    // AES_set_decrypt_key(key, 256, &aes_key);
+    // AES_cbc_encrypt(ciphertext, plaintext, AES_BLOCK_SIZE, &aes_key, iv, AES_DECRYPT);
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX_set_padding(ctx, 128);
+    EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+    EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, cipher_len);
+    EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
 }
 
 int main() {
@@ -25,6 +33,7 @@ int main() {
 
     unsigned char ciphertext[file_size];
     file.read((char *) ciphertext, file_size);
+    file.close();
 
     unsigned char key[32]= { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF,
                     0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10,
@@ -37,7 +46,7 @@ int main() {
         iv[i] = 0;
     }
 
-    aes_256_decrypt(ciphertext, key, iv, plaintext);
+    aes_256_decrypt(ciphertext, (int) file_size, key, iv, plaintext);
 
     /* Show the decrypted text */
     printf("Decrypted text: %s\n", plaintext);
